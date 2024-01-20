966099cf756fa802c37105d5bf7ea149c3085475
vehehgito <aryann2708@gmail.com>
2024-01-20 18:39:45 +0530

    Added sidebar component >> FinLit/Sidebar

--- src/Components/FinLit/FinLit.jsx
+++ src/Components/FinLit/FinLit.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react'
+
+import Sidebar from './Sidebar'
 import Budgeting from './Budgeting'
 
 function FinLit() {
   return (
-    <div className='min-h-screen mx-[5rem] pt-[7rem]'>
+    <div className='min-h-screen flex'>
+      <Sidebar/>
       <Budgeting/>
     </div>
   )
