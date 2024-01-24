3a326193e34e4c784c49eb6803f6c82a1554c629
muteenk <kundangarmuteen@gmail.com>
2024-01-25 00:12:52 +0530

    Minor updates

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -5,6 +5,26 @@ const UserContext = createContext();
 
 const UserProvider = ({children}) => {
     const [user, setUser] = useState(null);
+
+    const checkUserToken = async () => {
+        try {
+            const {data} = await axios.get('/me');
+            if (data.success === true) {
+                setUser(data.user);
+                // console.log(data.user)
+            }
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+    
+      useEffect(() => {
+          if (user === null) {
+              checkUserToken();            
+          }   
+      }, []);
+    
     
     return (
         <UserContext.Provider value={{user, setUser}}>
