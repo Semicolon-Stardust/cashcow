5cbf549ffabefaef23577776bb19ed93cae39e4a
vehehgito <aryann2708@gmail.com>
2024-01-26 02:55:50 +0530

    Partially fixed markdown parser

--- frontend/src/Components/FinLit/FinLit.jsx
+++ frontend/src/Components/FinLit/FinLit.jsx
@@ -1,26 +1,25 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
 import Markdown from "markdown-to-jsx"
 
-// const Post = () => {
-// 	const [postContent, setPostContent] = useState('');
+const Post = () => {
+	const [postContent, setPostContent] = useState('');
 
-// 	useEffect(() => {
-// 		import("../markdown/Budgeting.md")
-// 			.then(res => {
-// 				fetch(res.default)
-// 				.then(response => response.text())
-// 				.then(text => setPostContent(response))
-// 				.catch(err => console.log(err))
-// 			})
-// 	})
+	useEffect(() => {
+		import("../markdown/Budgeting.md")
+			.then(res => {
+				fetch(res.default)
+				.then(response => response.text())
+				.then(text => setPostContent(response))
+				.catch(err => console.log(err))
+			})
+	})
 
-// 	return (
-// 		<div className='post-wrapper'>
-// 			<Markdown>{postContent}</Markdown>
-// 		</div>
-// 	)
-// }
+	return (
+		<div className='post-wrapper'>
+			<Markdown>{postContent}</Markdown>
+		</div>
+	)
+}
 
 function FinLit() {
 	return (
@@ -29,3 +28,5 @@ function FinLit() {
 }
 
 export default FinLit
+
+
