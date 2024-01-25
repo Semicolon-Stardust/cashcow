627fc02dfb44ee0244bd183167e704a6d3d2f76c
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2024-01-26 00:16:25 +0530

    Navbar fix

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -142,7 +142,7 @@ function Login() {
                   type="submit"
                   className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                 >
-                  Sign up
+                  Login
                 </button>
               </div>
             </form>
