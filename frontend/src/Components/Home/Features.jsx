dc73d29ad2beada0cab8e5c0c7c1cd20d6d3d408
Rishit Jain <rishitjain4874@gmail.com>
2024-01-27 01:42:42 +0530

    Feature highlighted added

--- frontend/src/Components/Home/Features.jsx
+++ frontend/src/Components/Home/Features.jsx
@@ -32,8 +32,8 @@ function Features() {
         <svg className="flex-shrink-0 w-6 h-6 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg>
       </div>
       <div className="mt-5">
-        <h3 className="text-lg font-semibold text-gray-800 dark:text-white">Colaborative Budgenting and Expense Tracking</h3>
-        <p className="mt-1 text-gray-600 dark:text-gray-400">Transform the way you manage your finances</p>
+        <h3 className="text-lg font-semibold text-gray-800 dark:text-white">Educational Content</h3>
+        <p className="mt-1 text-gray-600 dark:text-gray-400">High-quality and easily digestible Financial educational content</p>
       </div>
     </div>
 
@@ -42,8 +42,8 @@ function Features() {
         <svg className="flex-shrink-0 w-6 h-6 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M14 9a2 2 0 0 1-2 2H6l-4 4V4c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2v5Z"/><path d="M18 9h2a2 2 0 0 1 2 2v11l-4-4h-6a2 2 0 0 1-2-2v-1"/></svg>
       </div>
       <div className="mt-5">
-        <h3 className="text-lg font-semibold text-gray-800 dark:text-white">24/7 Support</h3>
-        <p className="mt-1 text-gray-600 dark:text-gray-400">Contact us 24 hours a day, 7 days a week</p>
+        <h3 className="text-lg font-semibold text-gray-800 dark:text-white"> Colaborative Budgenting and Expense Tracking</h3>
+        <p className="mt-1 text-gray-600 dark:text-gray-400">Transform the way you manage your finances</p>
       </div>
     </div>
   </div>
